a5841c37dc473acfa665aea4a9cdd908f1be0be4
Mats Klingberg <mats.klingberg@bredband.net>
2008-02-05 02:01:19 +0100

    Expressions are now printed as s-expressions.

--- Expr.hs
+++ Expr.hs
@@ -10,5 +10,23 @@ data Expr = Symbol String           -- Scheme symbol
           | String String           -- String ("asdf")
           | Pair Expr Expr          -- Basic list building block
           | Null                    -- Empty list
-          deriving (Show)
+
+instance Show Expr where
+    show (Symbol x) = show x
+    show (Number x) = show x
+    show (Bool True) = "#t"
+    show (Bool False) = "#f"
+    show (String str) = "\"" ++ str ++ "\""
+    show p@(Pair _ _) = showPair p
+    show Null = "()"
+
+-- Show a pair
+-- This is a little tricky, since a pair with a pair in cdr should be shown in
+-- "list"-style, i.e. without the dot and without extra parantheses.
+showPair :: Expr -> String
+showPair p@(Pair _ _) = "(" ++ showPairNoParen p ++ ")" where
+    showPairNoParen :: Expr -> String
+    showPairNoParen (Pair car Null) = show car
+    showPairNoParen (Pair car cdr@(Pair _ _)) = show car ++ " " ++ showNoParen cdr
+    showPairNoParen (Pair car cdr) = show car ++ " . " ++ show cdr
 
